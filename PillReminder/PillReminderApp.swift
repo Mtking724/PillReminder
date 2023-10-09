4a0ad7133f26176ada97ff8b79aae095a5dccd95
motoki0724 <motokiokayasu@gmail.com>
2023-10-09 20:30:51 +0900

    Initial Commit

---
+++ PillReminder/PillReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillReminderApp.swift
+//  PillReminder
+//
+//  Created by Motoki Okayasu on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct PillReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
